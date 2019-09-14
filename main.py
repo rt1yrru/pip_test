7977b058abc5d986f795058f7950cc5054bb0836
siddht4 <dhar.75.ap@gmail.com>
2019-09-14 13:24:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 from pip._internal.utils.misc import get_installed_distributions
-_p=get_installed_distributions()
-print(_p[0].project_name)
-print(_p[0].version)
-print(_p[0].egg_name)
-print(_p[0].location)
+for _i in get_installed_distributions():
+	print('project_name : {} , version : {} , Path : {}'.format(_i.project_name,_i.version,_i.location))
